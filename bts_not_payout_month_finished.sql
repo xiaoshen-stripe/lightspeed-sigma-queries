7eda3534929dc46f5165ea87c3a33bbd43dbc396
Xiao Shen <xiaoshen@stripe.com>
2020-07-02 14:58:05 -0400

    bts not paid out in the month that just finished

---
+++ bts_not_payout_month_finished.sql
@@ -0,0 +1,26 @@
+select
+  id bt_id,
+  type bt_type,
+  reporting_category,
+  net amount,
+  currency,
+  created,
+  available_on,
+  source_id linked_model_jd,
+  description
+from
+  balance_transactions bts
+where
+  automatic_transfer_id is null
+  and bts.created >= date_add(
+    'month',
+    -1,
+    (date_trunc('month', current_date))
+  )
+  and bts.created < date_add(
+    'month',
+    0,
+    (date_trunc('month', current_date))
+  )
+   and status = 'available'
+     
